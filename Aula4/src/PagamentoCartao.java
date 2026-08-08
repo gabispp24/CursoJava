6f9f36261541e51e221a1268f75cffa5ec483283
GabriellePalucci <paluccigabrielle0@gmail.com>
2026-08-08 15:33:55 -0300

    Add files via upload

---
+++ Aula4/src/PagamentoCartao.java
@@ -0,0 +1,24 @@
+public class PagamentoCartao {
+    private int numeroDoCartao;
+    private int parcelas;
+
+    public int getNumeroDoCartao() {
+        return numeroDoCartao;
+    }
+
+    public void setNumeroDoCartao(int numeroDoCartao) {
+        this.numeroDoCartao = numeroDoCartao;
+    }
+
+    public int getParcelas() {
+        return parcelas;
+    }
+
+    public void setParcelas(int parcelas) {
+        this.parcelas = parcelas;
+    }
+
+
+}
+
+
